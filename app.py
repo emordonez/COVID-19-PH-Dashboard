7cf50847e64fecb301d1071618fa3cd92fc08f51
eric <emtordo@gmail.com>
2020-06-06 13:03:45 -0500

    data update

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def clean_dates(df):
 
 # Read data
 # TODO Write scripts to automatically download the latest data from Google Drive
-cases = pd.read_csv('DOH COVID Data Drop_ 20200530 - 04 Case Information.csv')
-aggs = pd.read_csv('DOH COVID Data Drop_ 20200530 - 07 Testing Aggregates.csv')
+cases = pd.read_csv('DOH COVID Data Drop_ 20200604 - 04 Case Information.csv')
+aggs = pd.read_csv('DOH COVID Data Drop_ 20200604 - 07 Testing Aggregates.csv')
 
 # Names management
 reg_df = pd.read_csv('assets/regions.csv').set_index('internal_name')
@@ -118,7 +118,7 @@ default_data = {
 CASES = f"{cases['CaseCode'].count():,}" + " cases"
 DEATHS = f"{cases[cases['HealthStatus'] == 'Died']['CaseCode'].count():,}" + " deaths"
 RECOVERIES = f"{cases[cases['HealthStatus'] == 'Recovered']['CaseCode'].count():,}" + " recoveries"
-CONFIRM_TO_DATE = "confirmed by the Department of Health as of May 30." # + date.today().strftime("%B %d") + "."
+CONFIRM_TO_DATE = "confirmed by the Department of Health as of Jun 4." # + date.today().strftime("%B %d") + "."
 
 TOTAL_TESTS = (f"{aggs.groupby('facility_name')['cumulative_unique_individuals'].max().sum():,}" +
     " people tested")
